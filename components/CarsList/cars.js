e27c5e47b66ad0b5d819af6640c26eeb539b59c2
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-02-03 04:07:40 +0530

    Header and Flatlist

---
+++ components/CarsList/cars.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        name: 'Model S',
+        tagline: 'Order Online for',
+        image: require('../../assets/images/ModelSRefresh.jpeg'),
+    },
+    {
+        name: 'Model Y',
+        tagline: 'Order Online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../assets/images/ModelY.jpeg'),
+    },
+    {
+        name: 'Model 3',
+        tagline: 'Order Online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../assets/images/Model3.jpeg'),
+    },
+    {
+        name: 'Model X',
+        tagline: 'Order Online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../assets/images/ModelX.jpeg'),
+    },
+    {
+        name: 'Lowest Cost Solar Panels in America',
+        tagline: 'Money-back guarantee',
+        taglineCTA: ' ',
+        image: require('../../assets/images/Panels.jpeg'),
+    },
+    {
+        name: 'Solar for New Roofs',
+        tagline: 'Solar Roof Costs Less Than a New Roof Plus Solar Panels',
+        taglineCTA: ' ',
+        image: require('../../assets/images/Roof.jpeg'),
+    },
+    {
+        name: 'Accessories',
+        tagline: ' ',
+        taglineCTA: ' ',
+        image: require('../../assets/images/Access.jpeg'),
+    },
+];
